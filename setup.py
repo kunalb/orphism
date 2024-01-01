cdcc29a161571315a5225dd3e55704fef095d5c9
Kunal Bhalla <bhalla.kunal@gmail.com>
2024-01-01 13:59:42 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="orphism",
-    version="0.0.1.dev3",
+    version="0.0.1.dev4",
     packages=find_packages(include=["orphism"]),
     package_data={
         "orphism": ["*.hy"],
